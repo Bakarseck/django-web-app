ab6de5849cebcf2dabceb59a6505e7e57eb99e6d
Bakarseck <86267786+Bakarseck@users.noreply.github.com>
2023-03-02 16:50:11 +0000

    f

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<h1>Listings</h1>')
+
+def contact(request):
+    return HttpResponse('<h1>Contact Us</h1>')
